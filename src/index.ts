3a9f4bffe336466c451fcaa755237181a42963b9
Dronicho <35956739+Dronicho@users.noreply.github.com>
2019-11-30 00:37:25 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-function greeter(person: string) {
-    alert('QQQHello, ' + person);
-}
 
-let user = 'Jane User';
-const btn = document.getElementById('test');
-
-btn.onclick = (e) => greeter(user);
