29f041c50982e02e3b805e02ea8cadea39bda4ae
Rain _ <hramil0120@gmail.com>
2019-12-07 03:26:49 +0300

    Fix indention according to eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 module.exports = (env, argv) => {
-    const path = require('path');
-    const webpack = require('webpack');
+  const path = require('path');
+  const webpack = require('webpack');
 
-    return {
-        entry: './src/index.ts',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.js'
+  return {
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      modules: [path.join(__dirname, 'src'), 'node_modules'],
+      extensions: ['.js', '.ts'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: 'ts-loader',
+            },
+            {
+              loader: 'eslint-loader',
+            },
+          ],
         },
-        resolve: {
-            modules: [path.join(__dirname, 'src'), 'node_modules'],
-            extensions: ['.js', '.ts']
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: [
-                        {
-                            loader: 'ts-loader'
-                        },
-                        {
-                            loader: 'eslint-loader'
-                        }
-                    ]
-                }
-            ]
-        }
-    }
-}
+      ],
+    },
+  };
+};
